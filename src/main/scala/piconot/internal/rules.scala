4a1902c1b516fdfe60cb96c80740bff61d8a6b0e
rjustice05 <rjustice@hmc.edu>
2016-03-02 20:33:22 -0800

    Wrote isWall and isEmpty

--- src/main/scala/piconot/internal/rules.scala
+++ src/main/scala/piconot/internal/rules.scala
@@ -6,33 +6,4 @@ import picolib.semantics._
 class rule (val name: String, subRules: Unit) {
      subRules
      
-     def isEmpty(dir1: MoveDirection = null,
-                 dir2: MoveDirection = null,
-                 dir3: MoveDirection = null,
-                 dir4: MoveDirection = null ) = {
-         
-     
-     }
-     def and () = Surroundings()
-     
-     def isWall(dir1: MoveDirection = null, 
-                dir2: MoveDirection = null,
-                dir3: MoveDirection = null,
-                dir4: MoveDirection = null ) = {
-         
-     }
-}
-
-class Conditional {
-    
-}
-
-object isEmpty extends Conditional{
-    
-}
-
-object isWall extends Conditional{
-    def and (other: Conditional) {
-        
-    }
 }
